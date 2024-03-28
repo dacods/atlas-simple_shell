99d131bc0fd12bf167d90cb662631ba220ecb718
LJThao <yengfong.thao@atlasschool.com>
2024-03-28 02:02:26 +0000

    changes

--- shell_input.c
+++ shell_input.c
@@ -8,24 +8,16 @@
 
 void shell_input(char *command, size_t size)
 {
-	char *line = NULL;
-	size_t len = 0;
-	ssize_t read;
-
-	read = getline(&line, &len, stdin);
-	if (read == -1)
-	{
-		if (feof(stdin))
-		{
-			printf("\n");
-			exit(EXIT_SUCCESS);
-		} else {
-			printf("Error while reading input.\n");
-			exit(EXIT_FAILURE);
+	if (fgets(command, size, stdin) == NULL)
+        {
+                if (feof(stdin))
+                {
+                        printf("\n");
+                        exit(EXIT_SUCCESS);
+                } else {
+                        printf("Error while reading input.\n");
+                        exit(EXIT_FAILURE);
                 }
         }
-	strncpy(command, line, size - 1);
-	command[size - 1] = '\0';
-
-	free(line);
+        command[strcspn(command, "\n")] = '\0';
 }
